29222379a86db50c697236c906725fdcb33c09bc
Mario Paulsen <mario.paulsen@lagarsoft.com>
2024-12-04 22:14:41 -0300

    Solution for day 4 part 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -39,75 +39,79 @@ public class Main {
         // the alphabet must be the characters read from the input
         alphabetSoup = Utils.convertToCharArray(rows);
 
-        int count = isPresent("XMAS");
-
-        System.out.println("Total words: " + total);
-        System.out.println("Count: " + count);
+        isPresent();
 
     }
-    
-    public static int isPresent(String word) {
+
+    public static char[][] getSubMatrix(char[][] matrix, int startRow, int startCol, int subRows, int subCols) {
+        // Initialize the submatrix
+        char[][] subMatrix = new char[subRows][subCols];
+
+        // Copy elements into the submatrix
+        for (int i = 0; i < subRows; i++) {
+            for (int j = 0; j < subCols; j++) {
+                subMatrix[i][j] = matrix[startRow + i][startCol + j];
+            }
+        }
+
+        return subMatrix;
+    }
+
+    public static int isPresent() {
         int count = 0;
         int iter = 0;
+        int countMatrix = 0;
         for (int i = 0; i < alphabetSoup.length; i++) {
             for (int j = 0; j < alphabetSoup[0].length; j++) {
                 iter++;
-                if (searchWordFromPosition(i, j, word, 0, true, DirectionEnum.FORWARD)) {
-                    count++;
+
+                if (i + 3 <= alphabetSoup.length && (j + 3 <= alphabetSoup[0].length)) {
+                    countMatrix++;
+                    char[][] matrix = getSubMatrix(alphabetSoup, i, j, 3, 3);
+                    if ((searchWordFromPositionWithMatrix(0, 0, "MAS", 0, DirectionEnum.DOWN_RIGHT, matrix) &&
+                        searchWordFromPositionWithMatrix(2, 0, "SAM", 0, DirectionEnum.DOWN_LEFT, matrix))
+                        ||
+                        (searchWordFromPositionWithMatrix(0, 0, "SAM", 0, DirectionEnum.DOWN_RIGHT, matrix) &&
+                            searchWordFromPositionWithMatrix(2, 0, "MAS", 0, DirectionEnum.DOWN_LEFT, matrix))
+                        ||
+                        (searchWordFromPositionWithMatrix(0, 0, "MAS", 0, DirectionEnum.DOWN_RIGHT, matrix) &&
+                            searchWordFromPositionWithMatrix(2, 0, "MAS", 0, DirectionEnum.DOWN_LEFT, matrix))
+                        ||
+                        (searchWordFromPositionWithMatrix(0, 0, "SAM", 0, DirectionEnum.DOWN_RIGHT, matrix) &&
+                            searchWordFromPositionWithMatrix(2, 0, "SAM", 0, DirectionEnum.DOWN_LEFT, matrix))
+                        ) {
+                        count++;
+                    }
                 }
+
             }
         }
         System.out.println("Iterations: " + iter);
+        System.out.println("Count Matrix: " + countMatrix);
+        System.out.println("Count: " + count);
         return count;
     }
 
-    private static boolean searchWordFromPosition(int x, int y, String word, int index, boolean firstTime, DirectionEnum direction) {
+    private static boolean searchWordFromPositionWithMatrix(int x, int y, String word, int index, DirectionEnum direction,
+        char[][] matrix) {
+
         if (index == word.length()) {
             return true;
         }
 
-        if (x < 0 || y < 0 || x >= alphabetSoup.length || y >= alphabetSoup[0].length || alphabetSoup[x][y] != word.charAt(index)) {
+        if (x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length || matrix[x][y] != word.charAt(index)) {
             return false;
         }
 
-        boolean result;
-        if (firstTime) {
-            boolean a = searchWordFromPosition(x + 1, y, word, index + 1, false, DirectionEnum.FORWARD);
-            boolean b = searchWordFromPosition(x - 1, y, word, index + 1, false, DirectionEnum.BACKWARD);
-            boolean c = searchWordFromPosition(x, y + 1, word, index + 1, false, DirectionEnum.UP);
-            boolean d = searchWordFromPosition(x, y - 1, word, index + 1, false, DirectionEnum.DOWN);
-
-            boolean e = searchWordFromPosition(x + 1, y + 1, word, index + 1, false, DirectionEnum.UP_RIGHT);
-            boolean f = searchWordFromPosition(x - 1, y - 1, word, index + 1, false, DirectionEnum.DOWN_LEFT);
-            boolean g = searchWordFromPosition(x - 1, y + 1, word, index + 1, false, DirectionEnum.UP_LEFT);
-            boolean h = searchWordFromPosition(x + 1, y - 1, word, index + 1, false, DirectionEnum.DOWN_RIGHT);
-
-            if (a) total++;
-            if (b) total++;
-            if (c) total++;
-            if (d) total++;
-            if (e) total++;
-            if (f) total++;
-            if (g) total++;
-            if (h) total++;
-
-            result = a || b || c || d || e || f || g || h;
-        } else {
-            result = switch (direction) {
-                case FORWARD -> searchWordFromPosition(x + 1, y, word, index + 1, false, DirectionEnum.FORWARD);
-                case BACKWARD -> searchWordFromPosition(x - 1, y, word, index + 1, false, DirectionEnum.BACKWARD);
-                case UP -> searchWordFromPosition(x, y + 1, word, index + 1, false, DirectionEnum.UP);
-                case DOWN -> searchWordFromPosition(x, y - 1, word, index + 1, false, DirectionEnum.DOWN);
-
-                case UP_RIGHT -> searchWordFromPosition(x + 1, y + 1, word, index + 1, false, DirectionEnum.UP_RIGHT);
-                case DOWN_LEFT -> searchWordFromPosition(x - 1, y - 1, word, index + 1, false, DirectionEnum.DOWN_LEFT);
-                case DOWN_RIGHT -> searchWordFromPosition(x + 1, y - 1, word, index + 1, false, DirectionEnum.DOWN_RIGHT);
-                case UP_LEFT -> searchWordFromPosition(x - 1, y + 1, word, index + 1, false, DirectionEnum.UP_LEFT);
-                default -> throw new RuntimeException("Invalid direction");
-            };
-
+        switch (direction) {
+            case DOWN_RIGHT -> {
+                return searchWordFromPositionWithMatrix(x + 1, y + 1, word, index + 1, DirectionEnum.DOWN_RIGHT, matrix);
+            }
+            case DOWN_LEFT -> {
+                return searchWordFromPositionWithMatrix(x - 1, y + 1, word, index + 1, DirectionEnum.DOWN_LEFT, matrix);
+            }
+            default -> throw new RuntimeException("Invalid direction");
         }
-        return result;
     }
 
 }
