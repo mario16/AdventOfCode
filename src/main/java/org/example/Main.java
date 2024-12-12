43b8c2a479214b66059de531048262f1722d63d9
Mario Paulsen <mario.paulsen@lagarsoft.com>
2024-12-12 17:21:32 -0300

    Solution day 11 part 2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,13 @@
 package org.example;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     private static final String ABSOLUTH_PATH_FOLDER = "/Users/mpaulsen/repos/lagarsoft/AdventOfCode/src/main/java/org/example/";
 
-    private static List<String> newStones = new ArrayList<>();
+    private static final Map<Integer, Map<String, Long>> cache = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -19,33 +17,63 @@ public class Main {
 //            String fileName = "sample-day11.txt";
             reader = new BufferedReader(new FileReader(ABSOLUTH_PATH_FOLDER + fileName));
             String line = reader.readLine();
-
-            while (line != null) {
-
-                var stones = line.split(" ");
-
-                int blinkCount = 25;
-                for (int i = 0; i < blinkCount; i++) {
-                    System.out.println("blink #" + (i+1+""));
-                    for (String part : stones) {
-                        newStones.addAll(applyRules(part));
-                    }
-                    stones = newStones.toArray(new String[0]);
-                    newStones.clear();
-                    System.out.println("stones: " + Arrays.toString(stones));
-                }
-
-                System.out.println("# stones: " + stones.length);
-
-                line = reader.readLine();
-            }
-
             reader.close();
+
+            var stones = line.split(" ");
+
+            int blinkCount = 75;
+            long result = blinkOnce(Arrays.asList(stones), blinkCount);
+
+            System.out.println("result: " + result);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static long blinkOnce(List<String> stones, int pendingBlinks) {
+        if (pendingBlinks == 0) {
+            return stones.size();
+        }
+        long result = 0;
+
+        if (!stones.isEmpty()) {
+            String nextValue = stones.get(0);
+            long value = getValue(pendingBlinks, nextValue);
+            if (value != -1) {
+                result += value;
+            } else {
+                List<String> newList = applyRules(nextValue);
+                result += blinkOnce(newList, pendingBlinks - 1);
+                saveValue(pendingBlinks, stones.get(0), result);
+            }
+
+            result += blinkOnce(stones.subList(1, stones.size()), pendingBlinks);
+        }
+        return result;
+
+    }
+
+    private static void saveValue(int pendingBlinks, String s, long result) {
+        if (!cache.containsKey(pendingBlinks)) {
+            cache.put(pendingBlinks, new HashMap<>());
+        } else {
+            if (cache.get(pendingBlinks).containsKey(s)) {
+                return;
+            }
+        }
+        cache.get(pendingBlinks).put(s, result);
+    }
+
+    private static long getValue(int pendingBlinks, String s) {
+        if (!cache.containsKey(pendingBlinks)) {
+            return -1;
+        }
+        if (!cache.get(pendingBlinks).containsKey(s)) {
+            return -1;
+        }
+        return cache.get(pendingBlinks).get(s);
     }
 
     private static List<String> applyRules(String stone) {
